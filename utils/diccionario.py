c79d5a68fa246a458c007a794e3b8d3c00b90219
tamnesiac <ygabriel@uninorte.edu.co>
2021-04-17 13:34:05 -0500

    added first encuesta and cleaningEncuesta for its proper cleaning

---
+++ utils/diccionario.py
@@ -0,0 +1,68 @@
+# Aquí estoy escribiendo todos los datos que creo que son manualmente digitados, cosas como estrato y grupo etnico para esta encuesta ya tienen formatos
+similitudes = {
+    'bogota' : 'bogotá',
+    'bogota dc': 'bogotá',
+    'bogota d.c.': 'bogotá',
+}
+
+datos = {
+    'departamento': {
+        'Antioquia',
+        'Atlántico', 
+        'Bogotá D.C.'
+        'Bolívar',
+        'Boyacá',
+        'Cauca', 
+        'Cesar'
+        'Córdoba',
+        'Cundinamarca'
+        'La Guajira',
+        'Magdalena',
+        'Meta',
+        'Nariño',
+        'Norte de Santander',
+        'Putumayo',
+        'Quindío',
+        'Risaralda', 
+        'Santander'
+        'Tolima',
+        'Valle del Cauca',
+    },
+    'municipio':{
+        'aguachica'
+        'barranquilla',
+        'bello',
+        'bogotá d.c.',
+        'bucaramanga',
+        'cali',
+        'cartagena',
+        'ciénaga de oro',
+        'cienaga',
+        'circasia',
+        'cota',
+        'cúcuta',
+        'dabeiba',
+        'duitama',
+        'envigado',
+        'fundación ',
+        'granada',
+        'ibagué',
+        'ipiales',
+        'marinilla',
+        'medellin',
+        'mocoa',
+        'monteria',
+        'mosquera',
+        'mutata',
+        'necocli',
+        'paipa',
+        'pasto',
+        'pereira',
+        'popayán',
+        'riohacha',
+        'santa marta',
+        'santiago de cali',
+        'turbaco',
+        'villavicencio',
+    }
+}
