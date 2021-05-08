5c9444780c2157881a23c1b973dd1623150c221f
tamnesiac <ygabriel@uninorte.edu.co>
2021-05-08 18:48:12 -0500

    deleted branched files.

--- src/data_cleaners/diccionario.py
+++
@@ -1,6 +0,0 @@
-# Aquí estoy escribiendo todos los datos que creo que son manualmente digitados, cosas como estrato y grupo etnico para esta encuesta ya tienen formatos
-municipio_parser = {
-    'bogota dc': 'bogota',
-    'bogota d.c.': 'bogota',
-    'santiago de cali': 'cali'
-}
