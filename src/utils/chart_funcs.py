f1a8ac9eff03ca88e2e36d7114071785d1ea3306
mariaf494 <mariaf494@gmail.com>
2021-06-03 02:34:41 -0500

    Update chart_funcs.py

--- src/utils/chart_funcs.py
+++ src/utils/chart_funcs.py
@@ -97,7 +97,7 @@ def scatter_chart(columna_unica=None, pivot=None, ejex=None, color=None, fila=No
 def categories_order(answers=None, pregunta=None):
     satisfaction = ["Nada satisfecho", "Un poco satisfecho", "Neutra",
                     "Muy satisfecho", "Totalmente satisfecho", "No puedo asistir"]
-    yes_no = ["Sí", "No"]
+    yes_no = ["SI", "NO"]
     de_acuerdo = ["Totalmente en desacuerdo", "En desacuerdo",
                   "Neutro", "De acuerdo", "Totalmente de acuerdo"]
     if len(set(satisfaction) - answers) < 2:
@@ -107,7 +107,8 @@ def categories_order(answers=None, pregunta=None):
     elif len(set(yes_no) - answers) < 2:
         cat_order = yes_no
     else:
-        cat_order = list(answers)
+        cat_order = [x for x in list(
+            answers) if x != 'No sé/No lo conozco']+['No sé/No lo conozco']
 
     category_orders = {pregunta: cat_order, "GENERO": ["F", "M"]}
 
