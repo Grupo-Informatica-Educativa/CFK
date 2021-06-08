e898028eb7c18bc1a49c431ec74fedf21f838453
tamnesiac <ygabriel@uninorte.edu.co>
2021-06-08 13:39:35 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-git pull origin publicrelease
-supervisorctl restart 2021
-=======
 git pull origin main
 supervisorctl restart 2021dev
->>>>>>> c8cb830388e2e299d3d9db7ece8d7bcce6e703dd
+
