379050f1247db61eb2803e4f2920c363763784cc
Miguel Nieto A <anietom@uninorte.edu.co>
2021-06-19 15:54:49 -0500

    formating code

--- app.py
+++ app.py
@@ -19,20 +19,28 @@ from src.preguntas.p1 import pregunta1
 app = MultiApp('CFK 2021')
 
 # Add all your application here
-app.add_app("Pretest Inicial", pretest_inicial.app,"Inicial")
-app.add_app("Posttest Inicial", posttest_inicial.app,"Inicial")
-app.add_app("Pre - Post tests Inicial", prepost_inicial.app,"Inicial")
-app.add_app("Pretest Avanzado", pretest_avanzado.app,"Avanzado")
-app.add_app("Posttest Avanzado", posttest_avanzado.app,"Avanzado")
-app.add_app("Pre - Post tests Avanzado", prepost_avanzado.app,"Avanzado")
-app.add_app("Encuesta Género a Mentores", encuesta_genero_mentores.app,"Mentores")
-app.add_app("Pretest Mentores", pretest_mentores.app,"Mentores")
+app.add_app("Pretest Inicial", pretest_inicial.app, "Inicial")
+app.add_app("Posttest Inicial", posttest_inicial.app, "Inicial")
+app.add_app("Pre - Post tests Inicial", prepost_inicial.app, "Inicial")
+
+app.add_app("Pretest Avanzado", pretest_avanzado.app, "Avanzado")
+app.add_app("Posttest Avanzado", posttest_avanzado.app, "Avanzado")
+app.add_app("Pre - Post tests Avanzado", prepost_avanzado.app, "Avanzado")
+
+app.add_app("Encuesta Género a Mentores",
+            encuesta_genero_mentores.app, "Mentores")
+app.add_app("Pretest Mentores", pretest_mentores.app, "Mentores")
 #app.add_app("Encuesta Caracterización", encuesta_caracterizacion.app)
 
-app.add_app("GreenTIC: Caracterización piloto beta",caracterizacion_piloto_beta.app,"Greentic")
-app.add_app("GreenTIC: Pretest piloto beta", pretest_piloto_beta.app,"Greentic")
-app.add_app("GreentTIC: Pretest piloto gamma",pretest_piloto_gamma.app,"Greentic")
-app.add_app("GreenTIC: Género y actitudes ambientales",genero_y_actitudes_ambientales_piloto_beta.app,"Greentic")
+app.add_app("GreenTIC: Caracterización piloto beta",
+            caracterizacion_piloto_beta.app, "Greentic")
+app.add_app("GreenTIC: Pretest piloto beta",
+            pretest_piloto_beta.app, "Greentic")
+app.add_app("GreentTIC: Pretest piloto gamma",
+            pretest_piloto_gamma.app, "Greentic")
+app.add_app("GreenTIC: Género y actitudes ambientales",
+            genero_y_actitudes_ambientales_piloto_beta.app, "Greentic")
 
+app.add_app("Graficador", ejemplo2020.app, "Herramientas")
 # The main app
 app.run()
--- multiapp.py
+++ multiapp.py
@@ -1,31 +1,30 @@
 """Frameworks for running multiple Streamlit applications as a single app.
 """
 import streamlit as st
-from src.pages import ejemplo2020
 
 
 class MultiApp:
 
     def __init__(self, page_title):
-        
         self.preguntas = []
         self.inicial = []
         self.avanzado = []
         self.mentores = []
         self.greentic = []
+        self.herramientas = []
         self.page_title = page_title
         st.set_page_config(
             page_title=page_title,
             layout="wide",
         )
 
-    def add_app(self, title, func,_type):
-        if  _type == "Inicial":
+    def add_app(self, title, func, _type):
+        if _type == "Inicial":
             self.inicial.append({
                 "title": title,
                 "function": func
             })
-        elif _type == "Avanzado":    
+        elif _type == "Avanzado":
             self.avanzado.append({
                 "title": title,
                 "function": func
@@ -45,25 +44,32 @@ class MultiApp:
                 "title": title,
                 "function": func
             })
+        elif _type == "Herramientas":
+            self.herramientas.append({
+                "title": title,
+                "function": func
+            })
 
     def run(self):
         st.sidebar.write(f'# {self.page_title}')
 
-        categorias = st.sidebar.radio("Secciones",["Herramientas","Cuestionario Inicial","Cuestionario Avanzado","Greentic","Mentores"])
- 
-        if categorias == "Herramientas":
-            pages = [{
-                    'title': 'Graficador',
-                    'function': ejemplo2020.app
-                }]
-            app = st.sidebar.radio("Seleccione herramienta: ", pages, format_func=lambda app: app['title'])
-        elif categorias == "Cuestionario Inicial":
-            app = st.sidebar.radio("Seleccione cuestionario: ", self.inicial, format_func=lambda app: app['title'])
-        elif categorias == "Cuestionario Avanzado":
-            app = st.sidebar.radio("Seleccione cuestionario: ", self.avanzado, format_func=lambda app: app['title'])
-        elif categorias == "Greentic":
-            app = st.sidebar.radio("Seleccione: ", self.greentic, format_func=lambda app: app['title'])
-        elif categorias == "Mentores":
-            app = st.sidebar.radio("Seleccione: ", self.mentores, format_func=lambda app: app['title'])
-        
+        categoria = st.sidebar.radio("Secciones",
+                                     ["Cuestionario Inicial", "Cuestionario Avanzado", "Greentic", "Mentores", "Herramientas"])
+
+        if categoria == "Herramientas":
+            app = st.sidebar.radio(
+                "Seleccione herramienta: ", self.herramientas, format_func=lambda app: app['title'])
+        elif categoria == "Cuestionario Inicial":
+            app = st.sidebar.radio(
+                "Seleccione cuestionario: ", self.inicial, format_func=lambda app: app['title'])
+        elif categoria == "Cuestionario Avanzado":
+            app = st.sidebar.radio(
+                "Seleccione cuestionario: ", self.avanzado, format_func=lambda app: app['title'])
+        elif categoria == "Greentic":
+            app = st.sidebar.radio(
+                "Seleccione: ", self.greentic, format_func=lambda app: app['title'])
+        elif categoria == "Mentores":
+            app = st.sidebar.radio(
+                "Seleccione: ", self.mentores, format_func=lambda app: app['title'])
+
         app['function']()
